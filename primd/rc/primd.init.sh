bbd994baf7f8158fa9f2c6f5d097740adb57fc67
admin <admin@my.(none)>
2014-10-29 23:11:44 +0900

    fix init script

--- primd/rc/primd.init.sh
+++ primd/rc/primd.init.sh
@@ -91,10 +91,10 @@ status() {
     local pid=`cat ${PID_FILE}`
     if checkpid ${pid}; then
         echo "${prog} is running... (pid: ${pid})"
-        return 0
+        retval=0
     else
         echo "${prog} is stopped"
-        return 1
+        retval=1
     fi
 }
 
